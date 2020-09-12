72575c297b3739da97c7a8ef183467af27c46901
WHDaniels <51680292+WHDaniels@users.noreply.github.com>
2020-09-12 01:01:38 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ import numpy as np
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
+from keras.models import Model
+from keras.layers import GRU, Input, Dense, TimeDistributed, Activation, RepeatVector, Bidirectional
+from keras.layers.embeddings import Embedding
+from keras.optimizers import Adam
+from keras.losses import sparse_categorical_crossentropy
+from tensorflow.python.keras.models import Sequential
+
 
 def loadData():
     enPhrases = []
@@ -18,71 +25,43 @@ def loadData():
     return enPhrases, esPhrases
 
 
-def showPhraseStats(enPhrases, esPhrases):
-    # FIX THIS LATER IF KEEPING IT
-    """
-    esWords = 0
-    enWords = 0
-
-    # gets the total number of english and spanish words
-    for i in range(0, len(enPhrases)):
-        enWords += len(enPhrases[i].split())
-        esWords += len(esPhrases[i].split())
-
-    nextEn = []
-    nextEs = []
-
-    # gets the entire list of unique words (not accurate)
-    for j in range(len(enPhrases)):
-        for word in enPhrases[j].split():
-            if word not in nextEn:
-                nextEn.append(word)
-
-        for word in esPhrases[j].split():
-            if word not in nextEs:
-                nextEs.append(word)
-
-    print(str(enWords) + " English words.\n" + str(len(nextEn)) + " unique English words.\n")
-    print(str(esWords) + " Spanish words.\n" + str(len(nextEs)) + " unique Spanish words.\n")
-    """
-
-
 def preprocess():
     # get lists of both english and spanish phrases
     enPhrases, esPhrases = loadData()
 
     # tokenizer setup
-    preEn = Tokenizer(char_level=False)
-    preEs = Tokenizer(char_level=False)
+    tkEn = Tokenizer(char_level=False)
+    tkEs = Tokenizer(char_level=False)
 
     # create index based on word frequency
-    preEn.fit_on_texts(enPhrases)
-    preEs.fit_on_texts(esPhrases)
+    tkEn.fit_on_texts(enPhrases)
+    tkEs.fit_on_texts(esPhrases)
 
     # transform index contents to integers
-    seqEn = preEn.texts_to_sequences(enPhrases)
-    seqEs = preEs.texts_to_sequences(esPhrases)
+    seqEn = tkEn.texts_to_sequences(enPhrases)
+    seqEs = tkEs.texts_to_sequences(esPhrases)
 
     # post-pad each index to the length of the largest phrase
     padEn = padding(seqEn)
     padEs = padding(seqEs)
 
-    # dimensions of the padded arrays
-    print(padEn.shape)
-    print(padEs.shape)
-
-    # reshape the second index to be 3-dimensional (sparse_categorical_crossentropy requires this)
+    # reshape the second index to be 3-dimensional (for sparse_categorical_crossentropy)
     padEs = padEs.reshape(*padEs.shape, 1)
-    print(padEs.shape)
 
     # amount of unique words in both languages
-    print(len(preEn.word_index))
-    print(len(preEs.word_index))
+    print(str(len(tkEn.word_index)))
+    print(str(len(tkEs.word_index)) + "\n\n")
+
+    newEn = padding(seqEn, 49)
+    newEn = newEn.reshape((-1, padEs.shape[-2], 1))
+
+    return padEs, tkEs, newEn
 
 
-def padding(sequence):
-    # gets the length of the largest phrase through a list comprehension
-    maxLength = max([len(phrase) for phrase in sequence])
+def padding(sequence, maxLength=None):
+    if maxLength is None:
+        # gets the length of the largest phrase through a list comprehension
+        maxLength = max([len(phrase) for phrase in sequence])
 
     # uses kera's padding function to pad zeros to the end of each index
     # up to the length of the largest phrase in that sequence
@@ -91,7 +70,24 @@ def padding(sequence):
     return paddedSeq
 
 
+def logitsToText(logits, tokenizer):
+    index_to_words = {id: word for word, id in tokenizer.word_index.items()}
+    index_to_words[0] = '<PAD>'
+
+    return ' '.join([index_to_words[prediction] for prediction in np.argmax(logits, 1)])
+
+
 if __name__ == '__main__':
-    en, es = loadData()
-    # showPhraseStats(en, es)
-    preprocess()
+    preEs, tkEs, enInput = preprocess()
+
+    learningRate = 1e-3
+    inputSeq = Input(enInput.shape[1:])
+    rnn = GRU(64, return_sequences=True)(inputSeq)
+    logits = TimeDistributed(Dense(len(tkEs.word_index)))(rnn)
+    model = Model(inputSeq, Activation('softmax')(logits))
+    model.compile(loss=sparse_categorical_crossentropy,
+                  optimizer=Adam(learningRate),
+                  metrics=['accuracy'])
+    model.fit(enInput, preEs, batch_size=1024, epochs=10, validation_split=0.2)
+
+    print(logitsToText(model.predict(enInput[:1][0]), tkEs))
