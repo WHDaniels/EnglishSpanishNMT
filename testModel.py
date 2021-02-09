6e3bc9e4487db3a17a66c41c0b64bfd704f6de05
WHDaniels <whdaniels3@gmail.com>
2021-02-09 00:30:20 -0600

    added BLEU score summation to analyze accuracy

--- testModel.py
+++ testModel.py
@@ -3,6 +3,7 @@ import numpy as np
 from keras_preprocessing.sequence import pad_sequences
 from tensorflow import keras
 import os
+from nltk.translate.bleu_score import sentence_bleu
 
 model = keras.models.load_model(os.getcwd() + '\\FINAL1')
 padEn, padEs = pickle.load(open('data//padEn.p', 'rb')), pickle.load(open('data//padEs.p', 'rb'))
@@ -17,14 +18,18 @@ def separatePhrases(testFile):
 
         for n, pairing in enumerate(testRead):
             separate = pairing.split("\t")
-            enPhrases.append(separate[0])
-            esPhrases.append(separate[1])
+            enPhrases.append(''.join(c for c in separate[0]
+                                     if c not in '!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n').lower())
+            esPhrases.append(''.join(c for c in separate[1]
+                                     if c not in '!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n').lower())
 
     return enPhrases, esPhrases
 
 
 def testAccuracy(enPhrases, esPhrases):
     translatedList = []
+    scoreCounter = 0
+    skipCounter = 0
 
     for n, phrase in enumerate(enPhrases):
         # mirrors the key to value relationship in tkEn.word_index
@@ -32,8 +37,17 @@ def testAccuracy(enPhrases, esPhrases):
         y_id_to_word[0] = '<PAD>'
 
         original = enPhrases[n]
-        sentence = ''.join(c for c in enPhrases[n] if c not in '!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n').lower()
+        sentence = enPhrases[n]
         # print(sentence)
+
+        skip = skipCounter
+        for word in sentence.split():
+            if word not in tkEn.word_index:
+                skipCounter += 1
+                break
+
+        if skipCounter > skip:
+            continue
 
         # sentence equals the list of ids that correspond to each word in sentence
         sentence = [tkEn.word_index[word] for word in sentence.split()]
@@ -44,11 +58,23 @@ def testAccuracy(enPhrases, esPhrases):
 
         predictions = model.predict(sentences, len(sentences))
 
-        print(original)
         translated = ' '.join([y_id_to_word[np.argmax(x)] for x in predictions[0]
                                if y_id_to_word[np.argmax(x)] != "<PAD>"])
-        print(translated)
+
         translatedList.append(translated)
+
+        print("Translated:", translated)
+        print("esPhrases[n]:", esPhrases[n])
+        candidate = [token for token in translated.split()]
+        reference = [token for token in esPhrases[n].split()]
+
+        score = sentence_bleu([reference], candidate, weights=(1, 0, 0, 0))
+        print(score)
+
+        scoreCounter += score
+
+    averageScore = scoreCounter / (len(enPhrases) - skipCounter)
+    print("\nThe average BLEU score for this model is ", averageScore)
 
 
 if __name__ == "__main__":
